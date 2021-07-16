eae5415b32566d297a1b25a5087e8950594adc22
kunalgupta711 <41870509+kunalgupta711@users.noreply.github.com>
2021-07-16 15:04:29 +0530

    sss

--- src/app/shared/notes.model.ts
+++ src/app/shared/notes.model.ts
@@ -2,4 +2,6 @@ export class Note {
   [x: string]: any;
     public title!: string;
     public body!: string;
+    public dateCreated: Date = new Date();
+    public dateUpdated: Date = new Date();
 }
